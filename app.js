cf7c3f775bdd3cc6db746d61922944d7d9235774
Imanol Bernardino Alonso <imanolbernardino92@gmail.com>
2019-11-26 12:59:31 +0100

    hola

--- app.js
+++ app.js
@@ -9,12 +9,7 @@ const app = express()
 // DB, middlewares, locals & debug
 const mongoose = require('./configs/mongoose.config')
 require('./configs/middlewares.config')(app)
-<<<<<<< HEAD
 require('./configs/preprocessor.config')(app)
-=======
-require('./configs/preprocessor.config')(app) 
-require('./configs/passport.config')(app)
->>>>>>> f0350da3191b572971af429fc38981905df7a512
 require('./configs/debug.config')
 
 
@@ -32,13 +27,8 @@ require('./passport')(app);
 // Base URL's
 app.use('/', require('./routes/index.routes'))
 app.use('/auth', require('./routes/auth.routes'))
-<<<<<<< HEAD
 app.use('/plans', require('./routes/plans.routes'))
 app.use('/profile', require('./routes/profile.routes'))
 
-=======
-app.use('/plans',require('./routes/plans.routes'))
-app.use('/profile',require('./routes/profile.routes'))
->>>>>>> f0350da3191b572971af429fc38981905df7a512
 
 module.exports = app;
