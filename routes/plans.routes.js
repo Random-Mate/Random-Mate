2fe7bca0c136c1a082e112d2cfecb2023808b90a
Imanol Bernardino Alonso <imanolbernardino92@gmail.com>
2019-11-27 20:43:35 +0100

    alfonso el que bebe ron sin coca-cola

--- routes/plans.routes.js
+++ routes/plans.routes.js
@@ -7,7 +7,7 @@ const Plans = require('../models/plans.model')
 router.get('/:id', (req, res) => {
   const idUser = req.params.id
   Plans.find()
-    .then(allPlans =>res.render('plans/plans',{plans:allPlans,user:idUser}))
+    .then(allPlans => res.render('plans/plans', { plans: allPlans, user: idUser }))
 })
 // router.post('/plans', (req, res) => {
 //   const { title, author , activity , atending , date , description , location } = req.body
@@ -17,51 +17,51 @@ router.get('/:id', (req, res) => {
 // })
 router.get('/newPlan/:id', (req, res) => res.render('plans/createPlan'))
 
-router.post('/newPlan/', (req, res) => {
-  const author= req.params.id
+router.post('/newPlan', (req, res) => {
   const { title, plan, date, description, location } = req.body
+  const author = req.user._id
   console.log(author)
 
-  if (!title|| !plan || !description || !location||!date) {
+  if (!title || !plan || !description || !location || !date) {
     res.render("plans/createPlan", {
       message: "Please enter all the fields"
     });
     return;
   }
 
-  Plans.create({title,plan,date,description,location,author})
+  Plans.create({ title, plan, date, description, location, author })
     .then(x => res.redirect('/'))
     .catch(err => console.log(err))
 })
 
-router.get('/join/:id', (req,res)=>res.render('plans/joinPlan'))
+router.get('/join/:id', (req, res) => res.render('plans/joinPlan'))
 
-router.post('/join/:id',(req,res)=>{
-    const plan = req.body
-    Plans.find(plan)
-        .then(allMatches => res.render('plans/joinPlan',{plans:allMatches}))
-        .catch(err => console.log(err))
+router.post('/join/:id', (req, res) => {
+  const plan = req.body
+  Plans.find(plan)
+    .then(allMatches => res.render('plans/joinPlan', { plans: allMatches }))
+    .catch(err => console.log(err))
 
 })
 router.get('/details/:id', (req, res) => {
   const planId = req.params.id
   Plans.findById(planId)
     .then(plan => res.render('plans/map', {
-      map:plan
+      map: plan
     }))
     .catch(err => console.log("Error consultando la BBDD: ", err))
-    
+
 })
-  
+
 
 
 router.get('/myPlans/:id', (req, res) => {
   const planId = req.params.id
-  Plans.find({ atending : planId })
+  Plans.find({ author: planId })
     .then(plans => res.render('plans/myPlan', { plan: plans }))
     .catch(err => console.log('error!!', err))
 })
-  module.exports = router;
+module.exports = router;
 
 router.get('/details/:id', (req, res) => {
   const plansId = req.params.id
