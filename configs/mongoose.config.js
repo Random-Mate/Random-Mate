cf64a5dd6f1104ba00248b0b69928ff5fd72d816
Imanol Bernardino Alonso <imanolbernardino92@gmail.com>
2020-01-08 16:55:11 +0100

    fixed index

--- configs/mongoose.config.js
+++ configs/mongoose.config.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose
-  .connect(`${process.env.DBLOCAL}`, { useNewUrlParser: true })
+  .connect(`${process.env.DB}`, { useNewUrlParser: true })
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err))
 
